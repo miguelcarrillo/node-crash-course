2c60e98c5c5235d8faae5f32c21c6c4a7b463a84
Miguel Carrillo <7063275+miguelcarrillo@users.noreply.github.com>
2021-11-05 19:58:16 -0500

    include ejs and css styles

--- app.js
+++ app.js
@@ -9,18 +9,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '},
+        {title: 'Mario finds star', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '},
+        {title: 'How to defeat Bowser', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '},
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create'});
 });
 
 // 404s
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404'});
 });
